ade9d1f6a3509b0b853b995c5263cd76ee48cfc5
navneetkumar22 <navneetsinghpanwar90@gmail.com>
2022-11-27 23:37:38 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => (
+    <header className="masthead bg-primary text-white text-center">
+        <div className="container d-flex align-items-center flex-column">
+            
+            <img className="masthead-avatar mb-5" src="https://avatars.githubusercontent.com/u/109900147?v=4" alt="..." />
+            
+            <h1 className="masthead-heading text-uppercase mb-0">FrontEnd Developer</h1>
+            
+            <div className="divider-custom divider-light">
+                <div className="divider-custom-line"></div>
+                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                <div className="divider-custom-line"></div>
+            </div>
+            
+            <p className="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+        </div>
+    </header>
+)
+
+
+export default Header;
